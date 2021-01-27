a4b870eb97f498da633ab3db14f669e79501bcbb
sigmund <mayitoukousigmund@gmail.com>
2021-01-27 19:07:53 +0100

    another commit

--- btre/settings.py
+++ btre/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '&kf^6pd6e*w=1o+&$)%18g(0owk05l3kf(iy2*qocbpx@(b+b='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['btr-project.herokuapp.com', '127.0.0.1']
 
