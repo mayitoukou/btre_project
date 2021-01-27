9bf035102b9ce99d877339f581627d6ef78b9d5e
sigmund <mayitoukousigmund@gmail.com>
2021-01-27 21:20:33 +0100

    Another commit

--- btre/settings.py
+++ btre/settings.py
@@ -92,7 +92,8 @@ DATABASES = {
         'NAME': 'btredb',
         'USER': 'genese',
         'PASSWORD': 'genese1995',
-        'HOST': '127.0.0.1'
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
