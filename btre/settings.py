1b816b055fb93feba328b552e4b683d87c349363
Sigmund <30848394+mayitoukou@users.noreply.github.com>
2021-07-14 14:26:35 +0100

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -157,10 +157,10 @@ MESSAGE_TAGS = {
 }
 
 # Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mayisficius@gmail.com'
-EMAIL_HOST_PASSWORD = 'ficiusgenese'
+EMAIL_HOST = ''
+EMAIL_PORT = 
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 
